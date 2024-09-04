11bb4d593d2421e373f1a7fc56dd4cb6fe04352d
mconcoba <mconcoba20@gmail.com>
2024-09-03 22:33:42 -0600

    feat(add): config to encrypt password

--- src/modules/users/repositories/user.repository.ts
+++ src/modules/users/repositories/user.repository.ts
@@ -3,6 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { BaseRepository } from 'src/_global/repositories/base-repository';
 import { Repository } from 'typeorm';
 import { UsersEntity } from '../entities/user.entity';
+import { UserDTO } from '../dto/user.dto';
+import * as bcrypt from 'bcrypt';
+import { ErrorManager } from 'src/util/error.manager';
 
 @Injectable()
 export class UserRepository extends BaseRepository<UsersEntity> {
@@ -13,7 +16,15 @@ export class UserRepository extends BaseRepository<UsersEntity> {
     super(userRepository); // Llamar al constructor del repositorio base
   }
 
-  // Aquí puedes agregar métodos específicos para UserEntity
+  async create(body: UserDTO): Promise<UsersEntity> {
+    try {
+      body.password = await bcrypt.hash(body.password, +process.env.HASH_SALT);
+      return this.userRepository.save(body);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
   async findByEmail(email: string): Promise<UsersEntity> {
     return this.userRepository.findOne({ where: { email } });
   }
