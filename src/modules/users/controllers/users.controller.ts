2699408fb2572d9ffc26366e3d91ecd2cb2d7bf9
mconcoba <mconcoba20@gmail.com>
2024-09-04 00:49:41 -0600

    feat(add): set roles in enpoints user

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -6,29 +6,39 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { UserDTO, UserUpdateDTO } from '../dto/user.dto';
+import { PublicAccess } from 'src/modules/auth/decorators/public.decorator';
+import { AuthGuard } from 'src/modules/auth/guards/auth.guard';
+import { RolesGuard } from 'src/modules/auth/guards/roles.guard';
+import { Roles } from 'src/modules/auth/decorators/roles.decorator';
 
 @Controller('users')
+@UseGuards(AuthGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly userService: UserService) {}
 
+  @Roles('ADMIN')
   @Post('create')
   public async create(@Body() body: UserDTO) {
     return await this.userService.createUser(body);
   }
 
+  @Roles('ADMIN')
   @Get('all')
   public async getAll() {
     return await this.userService.getUsers();
   }
 
+  @PublicAccess()
   @Get(':id')
   public async findUserById(@Param('id') id: number) {
     return await this.userService.getUserById(id);
   }
 
+  @Roles('ADMIN')
   @Put('edit/:id')
   public async updateUser(
     @Param('id') id: number,
@@ -37,6 +47,7 @@ export class UsersController {
     return await this.userService.updateUser(id, body);
   }
 
+  @Roles('ADMIN')
   @Delete('delete/:id')
   public async deleteUser(@Param('id') id: number) {
     return await this.userService.deleteUser(id);
