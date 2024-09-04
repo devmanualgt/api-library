b43c7a5141c811d83d6f5fee78f6f6e214c9663a
mconcoba <mconcoba20@gmail.com>
2024-09-04 00:29:28 -0600

    feat(add): guard to acces routes

--- src/modules/auth/interfaces/auth.interface.ts
+++ src/modules/auth/interfaces/auth.interface.ts
@@ -10,3 +10,18 @@ export interface AuthBody {
   username: string;
   password: string;
 }
+
+export interface AuthTokenResult {
+  role: string;
+  sub: string;
+  username: string;
+  iat: number;
+  exp: number;
+}
+
+export interface IUseToken {
+  role: string;
+  sub: string;
+  username: string;
+  isExprired: boolean;
+}
