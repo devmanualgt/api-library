c771de5e8e2f555aae1d48aea5a4f894e75bcb77
mconcoba <mconcoba20@gmail.com>
2024-09-03 21:02:37 -0600

    feat(add): setting to cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  app.enableCors(CORS);
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
+  console.log(`Application running on: ${await app.getUrl()}`);
 }
 bootstrap();
