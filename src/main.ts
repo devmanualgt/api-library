bfc2609968b72911173f34dbaec893481a6e57b3
mconcoba <mconcoba20@gmail.com>
2024-09-04 21:01:51 -0600

    feat(add): not use morgan

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as morgan from 'morgan';
+//import * as morgan from 'morgan';
 import { CORS } from './constants';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(morgan('dev'));
+  //app.use(morgan('dev'));
 
   app.useGlobalPipes(
     new ValidationPipe({
