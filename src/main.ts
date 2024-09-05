5190666118ecaf96b74c8d958bc433c080e97984
mconcoba <mconcoba20@gmail.com>
2024-09-05 16:05:57 -0600

    feat(add): format of responses

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-//import * as morgan from 'morgan';
+import * as morgan from 'morgan';
 import { CORS } from './constants';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { CustomExceptionFilter } from './util/error.manager';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,8 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
 
   app.enableCors(CORS);
+  app.useGlobalFilters(new CustomExceptionFilter()); // Aplicar el filtro globalmente
+
   app.setGlobalPrefix('api');
 
   await app.listen(3000);
--- src/util/response.manager.ts
+++ src/util/response.manager.ts
@@ -1,4 +1,8 @@
-export const response = (status: boolean, message: string, records?: any) => {
+export const response = (
+  status: boolean,
+  message: string | string[],
+  records?: any,
+) => {
   try {
     return {
       status,
